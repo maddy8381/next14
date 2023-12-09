50720aba89b9ef2ce469ce5a0267b51b19d6cfa9
Shubham Madankar <shubham.madankar@nium.com>
2023-12-09 11:15:34 +0530

    add api webhook to public routes

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,17 @@ import { authMiddleware } from "@clerk/nextjs";
 //For all the redirection related to login signup or authentication
 
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: [
+    "/",
+    "/api/webhook",
+    "/question/:id",
+    "/tags",
+    "/tags/:id",
+    "/profile/:id",
+    "/community",
+    "/jobs",
+  ],
+  ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
 });
 
 export const config = {
