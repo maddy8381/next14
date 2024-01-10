5b3c210840e2adb39cd9d1a8aae8990b4a405d1b
unknown <sgmadankar@gmail.com>
2024-01-10 23:50:22 +0530

    Voting system added and collection page implemented

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -1,11 +1,10 @@
-import { getTimestamp } from "@/lib/utils";
 import Link from "next/link";
 import React from "react";
-import Metric from "../shared/Metric";
 import RenderTag from "../shared/RenderTag";
-import { formatAndDivideNumber } from "../../lib/utils";
+import Metric from "../shared/Metric";
+import { formatAndDivideNumber, getTimestamp } from "@/lib/utils";
 
-interface Prop {
+interface QuestionProps {
   _id: string;
   title: string;
   tags: {
@@ -17,7 +16,7 @@ interface Prop {
     name: string;
     picture: string;
   };
-  upvotes: number;
+  upvotes: string[];
   views: number;
   answers: Array<object>;
   createdAt: Date;
@@ -32,12 +31,12 @@ const QuestionCard = ({
   views,
   answers,
   createdAt,
-}: Prop) => {
+}: QuestionProps) => {
   return (
     <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
       <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
         <div>
-          <span className="subtle-regular text-dark-400_light700 line-clamp-1 flex sm:hidden">
+          <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
             {getTimestamp(createdAt)}
           </span>
           <Link href={`/question/${_id}`}>
@@ -46,39 +45,44 @@ const QuestionCard = ({
             </h3>
           </Link>
         </div>
-        {/* If signed in add edit delete option */}
+
+        {/* If signed in add edit delete actions */}
       </div>
+
       <div className="mt-3.5 flex flex-wrap gap-2">
         {tags.map((tag) => (
           <RenderTag key={tag._id} _id={tag._id} name={tag.name} />
         ))}
       </div>
+
       <div className="flex-between mt-6 w-full flex-wrap gap-3">
         <Metric
           imgUrl={author.picture}
-          alt="User"
+          alt="user"
           value={author.name}
           title={` - asked ${getTimestamp(createdAt)}`}
           href={`/profile/${author._id}`}
+          isAuthor
           textStyles="body-medium text-dark400_light700"
         />
+
         <Metric
           imgUrl="/assets/icons/like.svg"
           alt="Upvotes"
-          value={formatAndDivideNumber(upvotes)}
+          value={formatAndDivideNumber(upvotes.length)}
           title=" Votes"
           textStyles="small-medium text-dark400_light800"
         />
         <Metric
           imgUrl="/assets/icons/message.svg"
-          alt="Message"
+          alt="message"
           value={formatAndDivideNumber(answers.length)}
           title=" Answers"
           textStyles="small-medium text-dark400_light800"
         />
         <Metric
           imgUrl="/assets/icons/eye.svg"
-          alt="Eye"
+          alt="eye"
           value={formatAndDivideNumber(views)}
           title=" Views"
           textStyles="small-medium text-dark400_light800"
--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,17 +1,23 @@
 "use server";
+
 import Question from "@/database/question.model";
 import Tag from "@/database/tag.model";
+import { connectToDatabase } from "../mongoose";
+import {
+  CreateQuestionParams,
+  GetQuestionByIdParams,
+  GetQuestionsParams,
+  QuestionVoteParams,
+} from "./shared.types";
 import User from "@/database/user.model";
 import { revalidatePath } from "next/cache";
-import { connectToDatabase } from "../mongoose";
-import { CreateQuestionParams, GetQuestionsParams } from "./shared.types";
 
 export async function getQuestions(params: GetQuestionsParams) {
   try {
     connectToDatabase();
 
     const questions = await Question.find({})
-      .populate({ path: "tags", model: Tag }) //Mongodb dont store value of this, only ref is stored. To get name  of tag from ref we use populate
+      .populate({ path: "tags", model: Tag })
       .populate({ path: "author", model: User })
       .sort({ createdAt: -1 });
 
@@ -25,6 +31,7 @@ export async function getQuestions(params: GetQuestionsParams) {
 export async function createQuestion(params: CreateQuestionParams) {
   try {
     connectToDatabase();
+
     const { title, content, tags, author, path } = params;
 
     // Create the question
@@ -36,28 +43,27 @@ export async function createQuestion(params: CreateQuestionParams) {
 
     const tagDocuments = [];
 
-    /* Create the tags or get them if they already exist
-    Its a many to many relationship between question and tags.
-      One question can have many tags
-    Ont tag can have multiple questions attached */
+    // Create the tags or get them if they already exist
     for (const tag of tags) {
       const existingTag = await Tag.findOneAndUpdate(
         { name: { $regex: new RegExp(`^${tag}$`, "i") } },
-        { $setOnInsert: { name: tag }, $push: { questions: question._id } }, //If it finds the  tag name in one of the tags of question, it push id of question into question array field of tags.
+        { $setOnInsert: { name: tag }, $push: { question: question._id } },
         { upsert: true, new: true }
       );
 
       tagDocuments.push(existingTag._id);
     }
 
-    //Attach the tags to the question.
     await Question.findByIdAndUpdate(question._id, {
       $push: { tags: { $each: tagDocuments } },
     });
+
+    // Create an interaction record for the user's ask_question action
+
+    // Increment author's reputation by +5 for creating a question
+
     revalidatePath(path);
-  } catch (error) {
-    console.log(error);
-  }
+  } catch (error) {}
 }
 
 export async function getQuestionById(params: GetQuestionByIdParams) {
@@ -75,6 +81,78 @@ export async function getQuestionById(params: GetQuestionByIdParams) {
       });
 
     return question;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function upvoteQuestion(params: QuestionVoteParams) {
+  try {
+    connectToDatabase();
+
+    const { questionId, userId, hasupVoted, hasdownVoted, path } = params;
+
+    let updateQuery = {};
+
+    if (hasupVoted) {
+      updateQuery = { $pull: { upvotes: userId } };
+    } else if (hasdownVoted) {
+      updateQuery = {
+        $pull: { downvotes: userId },
+        $push: { upvotes: userId },
+      };
+    } else {
+      updateQuery = { $addToSet: { upvotes: userId } };
+    }
+
+    const question = await Question.findByIdAndUpdate(questionId, updateQuery, {
+      new: true,
+    });
+
+    if (!question) {
+      throw new Error("Question not found");
+    }
+
+    // Increment author's reputation
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function downvoteQuestion(params: QuestionVoteParams) {
+  try {
+    connectToDatabase();
+
+    const { questionId, userId, hasupVoted, hasdownVoted, path } = params;
+
+    let updateQuery = {};
+
+    if (hasdownVoted) {
+      updateQuery = { $pull: { downvote: userId } };
+    } else if (hasupVoted) {
+      updateQuery = {
+        $pull: { upvotes: userId },
+        $push: { downvotes: userId },
+      };
+    } else {
+      updateQuery = { $addToSet: { downvotes: userId } };
+    }
+
+    const question = await Question.findByIdAndUpdate(questionId, updateQuery, {
+      new: true,
+    });
+
+    if (!question) {
+      throw new Error("Question not found");
+    }
+
+    // Increment author's reputation
+
+    revalidatePath(path);
   } catch (error) {
     console.log(error);
     throw error;
