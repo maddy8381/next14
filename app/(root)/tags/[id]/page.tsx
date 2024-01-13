af52000393cd30604bb77107bde37a4dcedf5319
unknown <sgmadankar@gmail.com>
2024-01-14 00:03:14 +0530

    Pagination added via search params in url

--- app/(root)/tags/[id]/page.tsx
+++ app/(root)/tags/[id]/page.tsx
@@ -1,14 +1,15 @@
 import QuestionCard from "@/components/cards/QuestionCard";
-import NoResult from "@/components/shared/NoResult/NoResult";
+import NoResult from "@/components/shared/NoResult";
+import Pagination from "@/components/shared/Pagination";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { IQuestion } from "@/database/question.model";
-import { getQuestionsByTagId } from "@/lib/actions/tag.action";
+import { getQuestionsByTagId } from "@/lib/actions/tag.actions";
 import { URLProps } from "@/types";
 
 const Page = async ({ params, searchParams }: URLProps) => {
   const result = await getQuestionsByTagId({
     tagId: params.id,
-    page: 1,
+    page: searchParams.page ? +searchParams.page : 1,
     searchQuery: searchParams.q,
   });
 
@@ -49,6 +50,13 @@ const Page = async ({ params, searchParams }: URLProps) => {
             linkTitle="Ask a Question"
           />
         )}
+      </div>
+
+      <div className="mt-10">
+        <Pagination
+          pageNumber={searchParams?.page ? +searchParams.page : 1}
+          isNext={result.isNext}
+        />
       </div>
     </>
   );
