a0586a542aafc23c9ee5c7bb57d696a01ddb8ec5
unknown <sgmadankar@gmail.com>
2024-01-14 01:11:32 +0530

    Implemented global search functionalit

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -2,9 +2,9 @@ import { SignedIn, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import GlobalSearch from "../search/GlobalSearch";
-import MobileNav from "./MobileNav";
 import Theme from "./Theme";
+import MobileNav from "./MobileNav";
+import GlobalSearch from "../search/GlobalSearch";
 
 const Navbar = () => {
   return (
@@ -14,15 +14,19 @@ const Navbar = () => {
           src="/assets/images/site-logo.svg"
           width={23}
           height={23}
-          alt="StackOverflow"
+          alt="DevFlow"
         />
+
         <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
           Dev <span className="text-primary-500">Overflow</span>
         </p>
       </Link>
+
       <GlobalSearch />
+
       <div className="flex-between gap-5">
         <Theme />
+
         <SignedIn>
           <UserButton
             afterSignOutUrl="/"
@@ -36,6 +40,7 @@ const Navbar = () => {
             }}
           />
         </SignedIn>
+
         <MobileNav />
       </div>
     </nav>
